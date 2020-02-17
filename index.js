17d452b5cc3e98cc650162b0a5ff1225122d947d
Asadbekinha <fayzulloevasadbek@gmail.com>
2020-02-17 17:54:23 +0500

    Add cool face API

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 
+const cool = require('cool-ascii-faces')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`)
 const TelegramBot = require('node-telegram-bot-api')
 const TOKEN = '646486697:AAEIjIA_X_z_R45z0rELGtj2rrEig-3EjZg';
 
